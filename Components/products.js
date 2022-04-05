0a75e32590fc02d2f8be3867c2d7cf2561be9645
Siddharth <officialsiddharthbisht@gmail.com>
2022-04-05 19:11:54 +0530

    added products section as module to import

---
+++ Components/products.js
@@ -0,0 +1,111 @@
+function products(){
+    return `
+    <h2>Featured Products</h2>
+    <p>Summer Collection New Modern Design</p>
+    <div class="product-container">
+        <div class="prod">
+            <img src="img/products/f1.jpg" alt="">
+            <div class="product-description">
+                <span>adidas</span>
+                <h5>Cartoon Astronaut T-Shirts</h5>
+                <div class="star">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <h4>Rs 600</h4>
+            </div>
+            <a href="#" class="cart"><i class="fal fa-shopping-cart cart"></i></a>
+        </div>
+        <div class="prod">
+            <img src="img/products/f2.jpg" alt="">
+            <div class="product-description">
+                <span>adidas</span>
+                <h5>Green Leave T-Shirts</h5>
+                <div class="star">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <h4>Rs 400</h4>
+            </div>
+            <a href="#" class="cart"><i class="fal fa-shopping-cart cart"></i></a>
+        </div>
+        <div class="prod">
+            <img src="img/products/f3.jpg" alt="">
+            <div class="product-description">
+                <span>adidas</span>
+                <h5>Red Flower T-Shirts</h5>
+                <div class="star">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <h4>Rs 800</h4>
+            </div>
+            <a href="#" class="cart"><i class="fal fa-shopping-cart cart"></i></a>
+        </div>
+        <div class="prod">
+            <img src="img/products/f4.jpg" alt="">
+            <div class="product-description">
+                <span>adidas</span>
+                <h5>Light Red T-Shirts</h5>
+                <div class="star">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <h4>Rs 550</h4>
+            </div>
+            <a href="#" class="cart"><i class="fal fa-shopping-cart cart"></i></a>
+        </div>
+        <div class="prod">
+            <img src="img/products/f5.jpg" alt="">
+            <div class="product-description">
+                <span>adidas</span>
+                <h5>Blossom T-Shirts</h5>
+                <div class="star">
+                    <i class="fas fa-star"></i>
+                </div>
+                <h4>Rs 200</h4>
+            </div>
+            <a href="#" class="cart"><i class="fal fa-shopping-cart cart"></i></a>
+        </div>
+        <div class="prod">
+            <img src="img/products/f6.jpg" alt="">
+            <div class="product-description">
+                <span>adidas</span>
+                <h5>Funky Multi T-Shirts</h5>
+                <div class="star">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <h4>Rs 1050</h4>
+            </div>
+            <a href="#" class="cart"><i class="fal fa-shopping-cart cart"></i></a>
+        </div>
+        <div class="prod">
+            <img src="img/products/f7.jpg" alt="">
+            <div class="product-description">
+                <span>adidas</span>
+                <h5>Wide Lower Blossom</h5>
+                <div class="star">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <h4>Rs 350</h4>
+            </div>
+            <a href="#" class="cart"><i class="fal fa-shopping-cart cart"></i></a>
+        </div>
+    </div>
+`
+}
+export default products;
