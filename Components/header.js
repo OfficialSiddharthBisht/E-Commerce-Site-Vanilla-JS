438ca9c074628578e1dc304415dd9c7175d13d21
Siddharth <officialsiddharthbisht@gmail.com>
2022-04-05 14:58:07 +0530

    converted the navbar as module to import

---
+++ Components/header.js
@@ -0,0 +1,17 @@
+function header(){
+    return `
+    <a href="#"><img src="img/logo.png" class= "logo" alt="Cara"></a>
+    <div>
+        <ul id="navbar">
+            <li><a class="active" href="index.html">Home</a></li>
+            <li><a href="Pages/shop.html">Shop</a></li>
+            <li><a href="Pages/blog.html">Blog</a></li>
+            <li><a href="Pages/about.html">About</a></li>
+            <li><a href="Pages/contact.html">Contact</a></li>
+            <li><a href="Pages/cart.html"><i class="far fa-shopping-bag"></i></a></li>
+        </ul>
+    </div>
+`;
+}
+
+export default header;
