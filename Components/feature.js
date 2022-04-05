50e9db1e5d376c37b9a9906b2b04ccc9d15c9242
Siddharth <officialsiddharthbisht@gmail.com>
2022-04-05 17:22:19 +0530

    made feature section as module

---
+++ Components/feature.js
@@ -0,0 +1,29 @@
+function feature(){
+    return `
+    <div class="feature-box">
+            <img src="img/features/f1.png" alt="">
+            <h6>Free Shipping</h6>
+        </div>
+        <div class="feature-box">
+            <img src="img/features/f2.png" alt="">
+            <h6>Online Order</h6>
+        </div>
+        <div class="feature-box">
+            <img src="img/features/f3.png" alt="">
+            <h6>Save Money</h6>
+        </div>
+        <div class="feature-box">
+            <img src="img/features/f4.png" alt="">
+            <h6>Promotions</h6>
+        </div>
+        <div class="feature-box">
+            <img src="img/features/f5.png" alt="">
+            <h6>Happy Selling</h6>
+        </div>
+        <div class="feature-box">
+            <img src="img/features/f6.png" alt="">
+            <h6>24 X 7 Support</h6>
+        </div>
+    `
+}
+export default feature;
